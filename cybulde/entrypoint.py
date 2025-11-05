9a2542e4fa0f080d5e33d8e67e8e6895316b5c71
CanerOren <canerorenn@gmail.com>
2025-11-05 16:13:59 +0300

    first commit

---
+++ cybulde/entrypoint.py
@@ -0,0 +1,9 @@
+from cybulde.configs_schemas.config_schema import Config
+from cybulde.utils.config_utils import get_config
+
+@get_config(config_path="../configs", config_name="config")
+def entrypoint(config: Config) -> None:
+    print(config)
+
+if __name__ == "__main__":
+    entrypoint() # type: ignore
---
+++ docker/scripts/startup-script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# konteyneri ayakta tut
+tail -f /dev/null
